0ec0c7df78034b2781c6d3b64622dbeec7652add
6ccb8v9nkd-eng <6ccb8v9nkd@privaterelay.appleid.com>
2026-09-20 01:40:37 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='skillhub-7-2-1-tech-admin-codes-v1';
-const SHELL=['./','./index.html','./styles.css?v=720','./app.js?v=720','./manifest.webmanifest?v=720','./icon-192-v718.png','./icon-512-v718.png'];
+const CACHE='skillhub-7-2-2-user-search-v1';
+const SHELL=['./','./index.html','./styles.css?v=722','./app.js?v=722','./manifest.webmanifest?v=722','./icon-192-v718.png','./icon-512-v718.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('skillhub-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
